d54cc2d619326e0861b38c5cb09ea22f2d710a5f
Fermin2049 <fermin2049@gmail.com>
2022-12-11 22:19:34 -0300

    1.2

--- proyectoWeb01/js/menu.js
+++ proyectoWeb01/js/menu.js
@@ -1,15 +1,17 @@
-const hamburguer = document.querySelector('.hamburguer')
-const menu = document.querySelector('.menu-navegacion')
+const hamburguer = document.querySelector('.hamburguer');
+const menu = document.querySelector('.menu-navegacion');
 
+hamburguer.addEventListener('click', () => {
+	menu.classList.toggle('spread');
+});
 
-hamburguer.addEventListener('click', ()=>{
-    menu.classList.toggle("spread")
-})
-
-window.addEventListener('click', e =>{
-    if(menu.classList.contains('spread') 
-        && e.target != menu && e.target != hamburguer){
-        console.log('cerrar')
-        menu.classList.toggle("spread")
-    }
-})
+window.addEventListener('click', (e) => {
+	if (
+		menu.classList.contains('spread') &&
+		e.target != menu &&
+		e.target != hamburguer
+	) {
+		console.log('cerrar');
+		menu.classList.toggle('spread');
+	}
+});
